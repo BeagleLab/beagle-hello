328e0e95ef9e5f0f9c37730f9abfc5bbfe54b052
Richard Littauer <richard.littauer@gmail.com>
2014-09-24 20:35:46 -0700

    A few more changes

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ function test () {
     'admiration in these grand scenes; but it is not possible to give an ' + 
     'adequate idea of the higher feelings of wonder, astonishment, and ' + 
     'devotion, which fill and elevate the mind. Charles Darwin';
-  console.log('\n' + darwinQuote + '\n\n' + 'All good.' + '\n');
-  return darwinQuote;
+  console.log('\n' + darwinQuote + '\n');
 }
 
-exports.module = 'beagle-hello'; // Should eventually be manifest-like object
+exports.moduleName = 'beagle-hello'; // Should eventually be manifest-like object
 exports.test = test;
 exports.html = '<h1>hello world!</h1><br />' + 
   '<p><i>From module beagle-hello.</i></p>';
